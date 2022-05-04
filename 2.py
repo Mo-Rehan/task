bc3a8dd5a2da6deecd77c296772e4b7060effd95
Mohammed Rehan Deshnoor <57529532+Mo-Rehan@users.noreply.github.com>
2022-05-04 16:49:39 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,17 @@
+# 2. Parse below output to form dictionary where output should be like {key: []}
+#     ex:     input:       in string
+# 	            device1   vlan1
+# 				device2   vlan2
+# 				device3   vlan1
+# 				device2   vlan3
+# 				device1   vlan4
+# 				device2   vlan1
+# 				device3   vlan6
+			
+# 			output: 
+# 			    {"device1": ["vlan1", "vlan4"], "device2": ["vlan2", "vlan1", "vlan3"], "device3": ["vlan1", "vlan6"]}
+
+
 
 str ="""device1   vlan1
 		device2   vlan2
